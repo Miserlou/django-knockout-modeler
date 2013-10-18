94ff0eb6f97439db134ad8f741d8972acb4c72c5
Rich Jones <rich@anomos.info>
2013-10-17 21:53:01 -0700

    fuck it, calling this 3.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-knockout-modeler',
-    version='0.1.0',
+    version='0.3.0',
     packages=['knockout_modeler'],
     install_requires=[
     ],
